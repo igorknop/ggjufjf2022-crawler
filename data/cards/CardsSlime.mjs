9ac76c162aff81eeb6256834f74e99d3fd1005de
Igor Knop <igorknop@gmail.com>
2022-01-29 14:12:02 -0300

    add effects text to enemy cards

--- data/cards/CardsSlime.mjs
+++ data/cards/CardsSlime.mjs
@@ -1,7 +1,7 @@
 export const CARDS_SLIME = [
-  { text: "Slime00", enemy: {name: 'Slime', hitPoints: 3}, player: {name: 'Cloth Gloves', effects:[{type:'attack', value:1}]}},
-  { text: "Slime01", enemy: {name: 'Slime', hitPoints: 3}, player: {name: 'Cloth Shoes', effects:[{type:'attack', value:2}]}},
-  { text: "Slime02", enemy: {name: 'Slime', hitPoints: 3}, player: {name: 'Cloth Hood', effects:[{type:'attack', value:3}]}},
-  { text: "Slime03", enemy: {name: 'Slime', hitPoints: 3}, player: {name: 'Cloth Shirt', effects:[{type:'attack', value:1},{type:'attack', value:1},]}},
-  { text: "Slime04", enemy: {name: 'Slime', hitPoints: 3}, player: {name: 'Cloth Leggings', effects:[{type:'attack', value:1}, {type:'defense', value:1},]}}
+  { text: "Slime00", enemy: {name: 'Slime', hitPoints: 3, effects:[{type:'attack', value: 1}]}, player: {name: 'Cloth Gloves', effects:[{type:'attack', value:1}]}},
+  { text: "Slime01", enemy: {name: 'Slime', hitPoints: 3, effects:[{type:'attack', value: 1}]}, player: {name: 'Cloth Shoes', effects:[{type:'attack', value:2}]}},
+  { text: "Slime02", enemy: {name: 'Slime', hitPoints: 3, effects:[{type:'attack', value: 1},{type:'defense', value: 1}]}, player: {name: 'Cloth Hood', effects:[{type:'attack', value:3}]}},
+  { text: "Slime03", enemy: {name: 'Slime', hitPoints: 3, effects:[{type:'attack', value: 1}, {type:'regeneration', value: 1}]}, player: {name: 'Cloth Shirt', effects:[{type:'attack', value:1},{type:'attack', value:1},]}},
+  { text: "Slime04", enemy: {name: 'Slime', hitPoints: 3, effects:[{type:'attack', value: 1}]}, player: {name: 'Cloth Leggings', effects:[{type:'attack', value:1}, {type:'defense', value:1},]}}
 ];
--- js/CrawlerCard.mjs
+++ js/CrawlerCard.mjs
@@ -44,12 +44,13 @@ export default class CrawlerCard extends Card {
         ctx.fillStyle = FRONT_COLOR;
         ctx.strokeRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
         ctx.font = '12px "Orbitron"';
-        ctx.fillText(this.player.name, this.x, this.y - this.w * 0.46, this.w*0.9);
+        ctx.fillText(this.player.name, this.x, this.y - this.w * 0.46, this.w * 0.9);
         ctx.fillStyle = FRONT_COLOR;
-        ctx.font="8px 'Orbitron'";
-        this.player.effects.forEach((effect, i) => {
-            ctx.fillText(`${effect.type} ${effect.value}`, this.x, this.y + this.w/2 - i * 12, this.w*0.9);
-        });
+        ctx.font = "8px 'Orbitron'";
+        for (let i = this.player.effects.length - 1; i >= 0; i--) {
+            const effect = this.player.effects[i];
+            ctx.fillText(`${effect.type} ${effect.value}`, this.x, this.y + this.w / 2 - i * 12, this.w * 0.9);
+        };
     }
     drawAsEnemy(ctx) {
         ctx.beginPath();
@@ -60,21 +61,28 @@ export default class CrawlerCard extends Card {
         ctx.fillStyle = BACKGROUND_COLOR;
         ctx.strokeRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
         ctx.font = '12px "Orbitron"';
-        ctx.fillText(this.enemy.name, this.x, this.y - this.h * 0.30, this.w*0.9);
+        ctx.fillText(this.enemy.name, this.x, this.y - this.h * 0.30, this.w * 0.9);
         for (let h = 0; h < this.enemy.hitPoints; h++) {
             ctx.fillStyle = BACKGROUND_COLOR;
-            ctx.fillRect(this.x  + h * 8 - this.w/2+4, this.y - this.h * 0.25, 6, 6);
+            ctx.fillRect(this.x + h * 8 - this.w / 2 + 4, this.y - this.h * 0.25, 6, 6);
         }
         for (let h = 0; h < this.enemy.damage; h++) {
             ctx.fillStyle = BACKGROUND_COLOR;
             ctx.beginPath();
-            ctx.ellipse(this.x  + this.w/2 - h * 8  - 8, this.y - this.h * 0.10, 3, 3, 0, Math.PI * 2, false);
+            ctx.ellipse(this.x + this.w / 2 - h * 8 - 8, this.y - this.h * 0.10, 3, 3, 0, Math.PI * 2, false);
             ctx.fill();
             ctx.closePath();
         }
+        ctx.fillStyle = BACKGROUND_COLOR;
+        ctx.font = "8px 'Orbitron'";
+        for (let i = this.enemy.effects?.length - 1; i >= 0; i--) {
+            const effect = this.enemy.effects[i];
+            ctx.fillText(`${effect.type} ${effect.value}`, this.x, this.y + this.w / 2 - i * 12, this.w * 0.9);
+        };
+
     }
 
-    getHint(){
+    getHint() {
         return `${this.player.name}
 ----------------
 ${this.enemy.name}`;
