98102d24e42ebfe450df6a55a3dec006ab20e202
Igor Knop <igorknop@gmail.com>
2022-01-30 18:38:29 -0300

    balancing level1

--- data/cards/Level1.mjs
+++ data/cards/Level1.mjs
@@ -8,9 +8,11 @@ import { CARDS_ZOMBIES } from "./CardsZombies.mjs";
 export default function generateLevel1() {
     const tier1 = [
         ...shuffleArray(
-            [...CARDS_GIANT_RATS.map(c => new CrawlerCard(c)),
+            [...CARDS_GIANT_RATS.slice(0,-1).map(c => new CrawlerCard(c)),
             ...CARDS_SLIMES.map(c => new CrawlerCard(c))]
         ),
+        new CrawlerCard(CARDS_GIANT_RATS[CARDS_GIANT_RATS.length-1]),
+        ,
         new CrawlerCard(CARDS_SPECIAL_ITEMS[0]),
         ,
         ...shuffleArray([
