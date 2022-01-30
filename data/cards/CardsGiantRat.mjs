879a9949c7e3f1cc20c61b9e5afdaefa8eaf7009
Igor Knop <igorknop@gmail.com>
2022-01-29 22:18:41 -0300

    Add set to player cards

--- data/cards/CardsGiantRat.mjs
+++ data/cards/CardsGiantRat.mjs
@@ -1,9 +1,9 @@
 import { EFFECT_ATTACK, EFFECT_DEFENSE, EFFECT_REGENERATION } from "../AllTimeConstants.mjs";
 
 export const CARDS_GIANT_RATS = [
-  { text: "GiantRat00", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}]}, player: {name: 'Bare Hands', effects:[{type:EFFECT_ATTACK, value:1}]}},
-  { text: "GiantRat01", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}]}, player: {name: 'Bare Feet', effects:[{type:EFFECT_ATTACK, value:1}]}},
-  { text: "GiantRat02", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1},{type:EFFECT_DEFENSE, value: 1}]}, player: {name: 'Bare Head', effects:[{type:EFFECT_ATTACK, value:1}]}},
-  { text: "GiantRat03", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}, {type:EFFECT_REGENERATION, value: 1}]}, player: {name: 'Bare Chest', effects:[{type:EFFECT_DEFENSE, value:1}]}},
-  { text: "GiantRat04", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}]}, player: {name: 'Waist Cloth', effects:[{type:EFFECT_ATTACK, value:1}, {type:EFFECT_DEFENSE, value:1},]}}
+  { text: "GiantRat00", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}]}, player: {name: 'Bare Hands', effects:[{type:EFFECT_ATTACK, value:1}], set:'A'}},
+  { text: "GiantRat01", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}]}, player: {name: 'Bare Feet', effects:[{type:EFFECT_ATTACK, value:1}], set:'A'}},
+  { text: "GiantRat02", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1},{type:EFFECT_DEFENSE, value: 1}]}, player: {name: 'Bare Head', effects:[{type:EFFECT_ATTACK, value:1}], set:'A'}},
+  { text: "GiantRat03", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}, {type:EFFECT_REGENERATION, value: 1}]}, player: {name: 'Bare Chest', effects:[{type:EFFECT_DEFENSE, value:1}], set:'A'}},
+  { text: "GiantRat04", enemy: {name: 'GiantRat', damage:0, hitPoints: 1, effects:[{type:EFFECT_ATTACK, value: 1}]}, player: {name: 'Waist Cloth', effects:[{type:EFFECT_ATTACK, value:1}, {type:EFFECT_DEFENSE, value:1},], set:'A'}}
 ];
